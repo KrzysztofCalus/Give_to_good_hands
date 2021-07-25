2ae7bc56859c08e057ade1f367ad391633c25e09
KrzysztofCalus <krzysztofcalus@gmail.com>
2021-07-25 18:53:17 +0200

    html data from database

--- charity_donation/charity/models.py
+++ charity_donation/charity/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+TYPES = (
+    ('fundacja', 'fundacja'),
+    ('organizacja pozarządowa', 'organizacja pozarządowa'),
+    ('zbiórka lokalna', 'zbiórka lokalna')
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    type = models.CharField(choices=TYPES, default='fundacja', max_length=56)
+    categories = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=128)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
--- charity_donation/charity/views.py
+++ charity_donation/charity/views.py
@@ -1,12 +1,25 @@
+from django.db.models import Sum
 from django.shortcuts import render
+
 
 # Create your views here.
 from django.views import View
 
+from charity.models import Donation, Institution
+
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "index.html")
+        quantity = Donation.objects.aggregate(Sum('quantity'))
+        institution = Institution.objects.count()
+        fundacja = Institution.objects.filter(type='fundacja')
+        organizacja = Institution.objects.filter(type='organizacja pozarządowa')
+        zbiorka = Institution.objects.filter(type='zbiórka lokalna')
+        return render(request, "index.html", {'quantity': quantity,
+                                              'institution': institution,
+                                              'fundacja': fundacja,
+                                              'organizacja': organizacja,
+                                              'zbiorka': zbiorka})
 
 
 class RegisterView(View):
